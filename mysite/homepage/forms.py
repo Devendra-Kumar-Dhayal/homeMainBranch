bbe15a4208021a7bb764a278e91f96140c04b5b4
Devendra-Kumar-Dhayal <101494090+Devendra-Kumar-Dhayal@users.noreply.github.com>
2023-06-19 16:53:18 +0530

    Reg Log system

---
+++ mysite/homepage/forms.py
@@ -0,0 +1,82 @@
+# <!-- Login From -->
+# <input type="email" placeholder="Enter your email" name="email" required />
+# <input type="password" placeholder="Enter your password" name="password" required />
+# <label for="check">Remember me</label>
+
+# <!-- Signup From -->
+
+# <input type="email" placeholder="Enter your email" required />
+# <input type="password" placeholder="Create password" required />
+# <input type="password" placeholder="Confirm password" required />
+
+from django import forms
+from django.contrib.auth import authenticate,get_user_model
+user=get_user_model()
+class UserLoginForm(forms.Form):
+    username=forms.CharField(label='Username',required = True,widget=forms.TextInput(attrs={"placeholder":"Username"}))
+    password=forms.CharField(widget=forms.PasswordInput(attrs={"placeholder":"Password"}), label='Password',required = True)
+    check = forms.BooleanField(label='Remember me',required=False)
+
+    class Meta:
+        model=user
+        fields=[
+          
+            'username',
+            'password',
+            
+        ]
+
+    def post(self, request, *args, **kwargs):
+        form = UserLoginForm(request.POST)
+        for field in form:
+            print("Field Error:", field.name,  field.errors)
+    def clean(self,*args,**kwargs):
+        username=self.cleaned_data.get('username')
+        password=self.cleaned_data.get('password')
+        check = self.cleaned_data.get('check')
+    
+
+        if username and password:
+            user=authenticate(username = username,password=password)
+
+            if not user:
+                raise forms.ValidationError('User Does Not Exist')
+            
+            if not user.check_password(password):
+                raise forms.ValidationError('Incorrect Password')
+        
+        return super(UserLoginForm, self).clean(*args,**kwargs)
+
+
+
+class UserRegisterForm(forms.ModelForm):
+    username=forms.CharField(label='Username',required = True,widget=forms.TextInput(attrs={"placeholder":"Username"}))
+
+    password1=forms.CharField(widget=forms.PasswordInput(attrs={"placeholder":"Password"}), label='Password',required = True)
+    password2=forms.CharField(widget=forms.PasswordInput(attrs={"placeholder":"Confirm Password"}), label='Confirm Password',required = True)
+
+    class Meta:
+        model=user
+        fields=[
+          
+            'username',
+            'password1',
+            'password2',
+        ]
+    def post(self, request, *args, **kwargs):
+        form = UserRegisterForm(request.POST)
+        for field in form:
+            print("Field Error:", field.name,  field.errors)
+    def clean(self,*args,**kwargs):
+        username = self.cleaned_data.get('username')
+        password1=self.cleaned_data.get('password1')
+        password2=self.cleaned_data.get('password2')
+
+        if password1!=password2:
+            raise forms.ValidationError("Passwords Don't Match")
+
+        # username_qs=user.objects.filter(username = username)
+        # if username_qs.exists():
+        #     raise forms.ValidationError("Username Already In Use")
+
+        return super(UserRegisterForm, self).clean(*args,**kwargs)
--- mysite/homepage/views.py
+++ mysite/homepage/views.py
@@ -1,11 +1,96 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import login, logout, authenticate,get_user_model
+from django.contrib import messages
+from django.contrib.auth.forms import UserChangeForm, AuthenticationForm
+from .forms import UserRegisterForm ,UserLoginForm
 
-def home(request):
-    return render(request, 'homepage/home.html')
 
 
-def rest(request):
-    return HttpResponse("rest") 
+
+
 
 # Create your views here.
+def home(request):
+    
+    form1 = UserLoginForm(request.POST or None )
+    form2 = UserRegisterForm(request.POST or None)
+    data = "NOt logged in"
+    if request.method == "POST" :
+       
+        print("form entry")
+        print(form2.post(request))
+        if form1.is_valid():
+            username = form1.cleaned_data.get('username')
+            password = form1.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            data = "logged up"
+        elif form2.is_valid():
+            user = form2.save(commit=False)
+            
+            password = form2.cleaned_data.get('password1')
+            user.set_password(password)
+            user.save()
+            new_user = authenticate(username=user.username, password=password)
+            login(request, new_user)
+            
+            data ="signed up"
+            
+
+    context = {
+        'data': data,
+        'form1':form1,
+        'form2':form2
+    }
+    return render(request, 'homepage/home.html',context)
+
+
+
+  
+
+
+# def home(request):
+#     return render(request,"home.html")
+
+def login_view(request):
+    next = request.GET.get('next')
+    form = UserLoginForm(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        if next:
+            return redirect(next)
+        return redirect('/')
+
+    context = {
+        'form': form,
+    }
+    return render(request, "login.html", context)
+
+
+def register_view(request):
+    next = request.GET.get('next')
+    form = UserRegisterForm(request.POST or None)
+    if form.is_valid():
+        user = form.save(commit=False)
+        password = form.cleaned_data.get('password')
+        user.set_password(password)
+        user.save()
+        new_user = authenticate(username=user.username, password=password)
+        login(request, new_user)
+        if next:
+            return redirect(next)
+        return redirect('/')
+
+    context = {
+        'form': form,
+    }
+    return render(request, "signup.html", context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
