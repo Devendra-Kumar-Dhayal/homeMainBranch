43bae51613000d1e1eb9452ba456e2115fcca502
Devendra-Kumar-Dhayal <101494090+Devendra-Kumar-Dhayal@users.noreply.github.com>
2023-06-20 07:26:16 +0530

    removed unnecessary files

--- mysite/login/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.home),
-    path('login/', views.login_view, name="login"),
-    path('register/', views.register_view, name="register"),
-    path('logout/', views.logout_view,  name="logout")
-]
