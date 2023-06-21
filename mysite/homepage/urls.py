7d2e7894e1765b65cbb2da8b6e7260f8201df819
DivyamShma <divyamsharma511@gmail.com>
2023-06-21 19:31:00 +0530

    bug fixed due to password functionality

--- mysite/homepage/urls.py
+++ mysite/homepage/urls.py
@@ -8,10 +8,11 @@ urlpatterns = [
     path('logout/',views.logout_view),
     path('recoverpassword/',ResetPasswordView.as_view()),
     path('new_page.html/<str:pk>',views.rest),
-    path('password-reset-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view('homepage/password_reset_confirm.html'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(template_name='homepage/password_reset_confirm.html'),
          name='password_reset_confirm'),
     path('password-reset-complete/',
-         auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+         auth_views.PasswordResetCompleteView.as_view(template_name='homepage/password_reset_complete.html'),
          name='password_reset_complete'),
     path('new_page.html/<str:pk>',views.rest),
 ]
