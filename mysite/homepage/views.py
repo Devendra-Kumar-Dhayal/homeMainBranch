d7c141ead59f6b4bd392153b51f78eca93e435c9
Devendra-Kumar-Dhayal <101494090+Devendra-Kumar-Dhayal@users.noreply.github.com>
2023-06-21 16:49:59 +0530

    login error shows

--- mysite/homepage/views.py
+++ mysite/homepage/views.py
@@ -6,7 +6,10 @@ from django.contrib.auth.forms import UserChangeForm, AuthenticationForm
 from .forms import UserRegisterForm ,UserLoginForm
 from .models import GameInfo
 from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth.models import User
 
+def username_exists(username):
+    return User.objects.filter(username=username).exists()
 
 def rest(request, pk):
     return render(request, "new_page.html") 
@@ -24,37 +27,58 @@ def home(request):
     form1 = UserLoginForm(request.POST or None )
     form2 = UserRegisterForm(request.POST or None)
     data = "NOt logged in"
+    flag = False
+    sFlag = False
+    autherror=''
     if request.method == "POST" :
        
         # print("form entry")
         # print(form2.post(request))
+        
         if form1.is_valid():
             username = form1.cleaned_data.get('username')
             password = form1.cleaned_data.get('password')
             user = authenticate(username=username, password=password)
+            # data = username
             login(request, user)
             data = "logged up"
             # return redirect('/')
+        elif str(form1.errors)!='<ul class="errorlist"><li>username<ul class="errorlist"><li>This field is required.</li></ul></li><li>password<ul class="errorlist"><li>This field is required.</li></ul></li></ul>':
+            flag = True
         
         elif form2.is_valid():
+            
             user = form2.save(commit=False)
             username = form2.cleaned_data.get('newuser')
             user.username=username
             password = form2.cleaned_data.get('password1')
-            user.set_password(password)
-            user.save()
-            new_user = authenticate(username=user.username, password=password)
-            login(request, new_user)
+            if username_exists(username):
+                autherror= 'username is used'
+                sFlag=True
             
-            data ="signed up"
+            else:
+                user.set_password(password)
+                user.save()
+                new_user = authenticate(username=user.username, password=password)
+                login(request, new_user)
+                
+                data ="signed up"
             # return redirect('/')
+        else :
+            sFlag=True
+        print(form2.errors)
+        print(flag)
+        print(sFlag)
             
 
     context = {
         'data': data,
         'form1':form1,
         'form2':form2,
-        'game':game
+        'game':game,
+        'retryLogin':flag,
+        'retrySignup':sFlag,
+        'autherror':autherror
     }
     return render(request, 'homepage/home.html',context)
 
